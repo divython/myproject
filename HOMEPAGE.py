dd4059c5783c78aa8fc05981cb38099f1efdb2df
Divyanshu Chaudhary <84345836+divython@users.noreply.github.com>
2023-10-02 22:47:48 +0530

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -6,20 +6,36 @@ Created on Sun Oct  1 12:44:00 2023
 """
 
 import streamlit as st
+
+# Set page title and icon
 st.set_page_config(
-    page_title='my project for samsung innovation campus',
+    page_title='My Project for Samsung Innovation Campus',
     page_icon='🧠'
-    )
-st.title("main page")
-st.sidebar.success("select above page")
-import os
+)
 
-file_path = os.path.join("pages", "mycifar10.keras")
+# Main page title
+st.title("Main Page")
 
-if os.path.exists(file_path):
-    with open(file_path, "rb") as file:
-        # Perform actions with the file
-        pass
-else:
-    print("File not found at:", file_path)
+# Sidebar with a success message
+st.sidebar.success("Select an option from the sidebar")
 
+# Description
+description = """
+This Streamlit app showcases my project for the Samsung Innovation Campus. 
+Feel free to explore and interact with the different features and functionalities.
+"""
+
+# Display the description
+st.write(description)
+
+# Paragraph
+paragraph = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec erat ut leo pharetra varius.
+Duis at metus nec urna lobortis malesuada. Sed euismod lectus ut elit feugiat, nec accumsan tortor varius.
+Praesent ac ligula euismod, sollicitudin purus at, varius orci. Nam fringilla orci eget dui ultricies, 
+et bibendum turpis aliquet. Sed vel facilisis libero. Curabitur non neque in turpis vestibulum 
+euismod ut id libero. Nullam at tincidunt augue, vel venenatis sapien.
+"""
+
+# Display the paragraph
+st.write(paragraph)
