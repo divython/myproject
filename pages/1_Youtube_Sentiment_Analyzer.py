e544b63c96524f08730ba1f31cac75a5f108d2d6
Divyanshu Chaudhary <84345836+divython@users.noreply.github.com>
2023-10-02 23:04:35 +0530

    Update 1_Youtube_Sentiment_Analyzer.py

--- pages/1_Youtube_Sentiment_Analyzer.py
+++ pages/1_Youtube_Sentiment_Analyzer.py
@@ -206,7 +206,7 @@ def plot_sentiment_distribution(sentiment_scores):
 # Define the Streamlit app
 st.title('YouTube Video Analyzer')
 st.set_option('deprecation.showPyplotGlobalUse', False)
-description ="""Project 2 focuses on sentiment analysis of YouTube 
+description ="""It focuses on sentiment analysis of YouTube 
 comments extracted from provided URLs. The application categorizes comments into positive, 
 negative, or neutral sentiments using natural language processing. 
 This analysis helps gauge public sentiment toward specific YouTube content."""
